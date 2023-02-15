3fedf357b2b6ba10af866177d130b40aaec40eea
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2023-02-15 09:08:47 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -4,7 +4,15 @@ import torch
 import importlib
 import options
 from util import log
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+# python3 evaluate.py --group=strayscanner or iphone
+#                     --model=barf
+#                     --yaml=barf_strayscanner or iphone
+#                     --name=statue_2 --data.scene=statue
+#                     --data.val_sub= --resume
+#
 def main():
 
     log.process(os.getpid())
@@ -20,26 +28,14 @@ def main():
         m.load_dataset(opt,eval_split="test")
         m.build_networks(opt)
 
-        # if opt.model=="barf":
-            # m.generate_optim_pose_oneNall(opt) # 논문에 그릴 포즈 select 하기 위한 파트
-            # m.generate_optim_pose_onebyone(opt)  # 논문에 그릴 포즈 select 하기 위한 파트
-            # m.generate_optim_pose(opt) #  논문에 넣을 select한 포즈만 그리기 위한 파트
-            # m.generate_videos_pose(opt)
-        #     m.restore_checkpoint(opt)
-        #     m.evaluate_ckt(opt)
-
-
+        if opt.model=="barf":
+            m.generate_videos_pose(opt)
         m.restore_checkpoint(opt)
-        if opt.data.dataset in ["blender","llff","arkit","iphone"]:
-            m.evaluate_full(opt)
-        #
+        if opt.data.dataset in ["blender","llff","arkit","iphone","strayscanner"]:
+            m.evaluate_full(opt)   # test pose novel_view
+
         # #novel_view synthesis
-        # m.generate_videos_synthesis(opt) # novel_view
-        # m.generate_videos_synthesis_origin(opt) # origin novel_view synthesis code
-        """
-            novel_view : GT 포즈 범위에서 novel view 생성
-            origin_novel_view : train 과정에서 optimize한 포즈 범위에서 novel_view 생성
-        """
+        m.generate_videos_synthesis(opt) # load test pose novel_view   : make novel_view
 
 if __name__=="__main__":
     main()
